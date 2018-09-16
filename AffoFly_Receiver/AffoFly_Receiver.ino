e7250f584e00a066ef24d4bc2b9aa719e32dccab
utsxumiao <utsxumiao@gmail.com>
2018-09-16 17:50:38 +1000

    Initiated Receiver sketch
    Someone says use the Arduino timer to convert data into PPM is not acurate, also the FC has to convert PPM back to data then PWM to motor, it might not work properly. For this sketch I use it mainly testing the TX signal and planned make it use for simulator.

---
+++ AffoFly_Receiver/AffoFly_Receiver.ino
@@ -0,0 +1,236 @@
+#include <EEPROM.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include "printf.h"
+
+//==== System Setting =============================================//
+#define VERSION_NUMBER     "0.012"
+//#define DEBUG_MODE  1
+#define CLOCK_MULTIPLIER 1 // set this to 2 if you are using a 16MHz arduino, leave as 1 for an 8MHz arduino
+//=======================================================================//
+
+//==== Pin Definition ===================================================//
+#define PPM_PIN       2 // It has to be 2 unless you also update setPPM() and ISR
+#define LED_PIN       3
+#define NRF_CE_PIN    7
+#define NRF_CSN_PIN   8
+//=======================================================================//
+
+//==== Global Constant & Variable =======================================//
+const uint16_t PPM_FRAME_LENGTH = 2700;
+const uint16_t PPM_PULSE_LENGTH = 400;
+const uint8_t CHANNEL_COUNT = 10;
+const uint16_t RADIO_TX_ID = 1005;
+const uint16_t RADIO_RX_ID = 1;
+const uint8_t RADIO_CHANNEL = 101;
+const uint64_t RADIO_PIPE_OUT = 0xE8E8F0F0E1LL;
+
+const uint16_t RADIO_SIGNAL_TIMEOUT = 500;
+unsigned long RadioSignalLastMillis = 0;
+const uint8_t LED_FLASH_INTERVAL = 20;
+unsigned long LedFlashLastMillis = 0;
+const uint16_t RATE_REFRESH_INTERVAL = 1000;
+unsigned long RateRefreshLastMillis = 0;
+
+bool HasSignal = false;
+bool LedState = false;
+
+RF24 radio(NRF_CE_PIN, NRF_CSN_PIN);
+
+struct MyData {
+  uint16_t TxId;
+  //uint16_t RxId;
+  uint16_t Throttle;
+  uint16_t Yaw;
+  uint16_t Pitch;
+  uint16_t Roll;
+  uint16_t Aux1;
+  uint16_t Aux2;
+  uint16_t Aux3;
+  uint16_t Aux4;
+  uint16_t Aux5;
+  uint16_t Aux6;
+};
+MyData data;
+
+uint16_t PPM[CHANNEL_COUNT];
+
+#ifdef DEBUG_MODE
+uint16_t PackageCount = 0;
+uint16_t PackageRate = 0;
+uint16_t LoopCount = 0;
+uint16_t LoopRate = 0;
+#endif
+//=======================================================================//
+
+void setup() {
+#ifdef DEBUG_MODE
+  Serial.begin(115200);
+#endif
+  pinMode(PPM_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  radio.begin();
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setChannel(RADIO_CHANNEL);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setAutoAck(false);
+  radio.openReadingPipe(1, RADIO_PIPE_OUT);
+  radio.startListening();
+#ifdef DEBUG_MODE
+  printf_begin();
+  radio.printDetails();
+#endif
+
+  setupPPM();
+  resetData();
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  receiveData(currentMillis);
+  checkSignal(currentMillis);
+  flashLed(currentMillis);
+#ifdef DEBUG_MODE
+  showRate(currentMillis);
+  LoopCount++;
+#endif
+}
+
+#ifdef DEBUG_MODE
+void showRate(unsigned long currentMillis) {
+  if (currentMillis - RateRefreshLastMillis >= RATE_REFRESH_INTERVAL) {
+    RateRefreshLastMillis = currentMillis;
+    PackageRate = PackageCount;
+    LoopRate = LoopCount;
+    PackageCount = 0;
+    LoopCount = 0;
+    Serial.print(PackageRate);  Serial.print(" / ");  Serial.println(LoopRate);
+  }
+}
+#endif
+
+void receiveData(unsigned long currentMillis) {
+  while(radio.available()){
+    radio.read(&data, sizeof(MyData));
+    if(data.TxId == RADIO_TX_ID) {
+      HasSignal = true;
+      RadioSignalLastMillis = currentMillis;
+#ifdef DEBUG_MODE
+      PackageCount++;
+#endif
+      setPPMValuesFromData();
+    }
+//#ifdef DEBUG_MODE
+//    Serial.print("TxId: "); Serial.print(data.TxId);  Serial.print("    ");
+//    Serial.print("Throttle: "); Serial.print(data.Throttle);  Serial.print("    ");
+//    Serial.print("Yaw: ");      Serial.print(data.Yaw);       Serial.print("    ");
+//    Serial.print("Pitch: ");    Serial.print(data.Pitch);     Serial.print("    ");
+//    Serial.print("Roll: ");     Serial.print(data.Roll);      Serial.print("    ");
+//    Serial.print("Aux1: ");     Serial.print(data.Aux1);      Serial.print("    ");
+//    Serial.print("Aux2: ");     Serial.print(data.Aux2);      Serial.print("    ");
+//    Serial.print("Aux3: ");     Serial.print(data.Aux3);      Serial.print("    ");
+//    Serial.print("Aux4: ");     Serial.print(data.Aux4);      Serial.print("    ");
+//    Serial.print("Aux5: ");     Serial.print(data.Aux5);      Serial.print("    ");
+//    Serial.print("Aux6: ");     Serial.print(data.Aux6);      Serial.print("\n");
+//#endif
+  }
+}
+
+void checkSignal(unsigned long currentMillis) {
+  if(currentMillis - RadioSignalLastMillis >= RADIO_SIGNAL_TIMEOUT) {
+    HasSignal = false;
+    resetData();
+  }
+}
+
+void flashLed(unsigned long currentMillis) {
+  if (currentMillis - LedFlashLastMillis >= LED_FLASH_INTERVAL) {
+    LedFlashLastMillis = currentMillis;
+    if (HasSignal) {
+      LedState = !LedState;
+    } else {
+      if (LedState) LedState = false;
+    }
+    digitalWrite(LED_PIN, LedState);
+  }
+}
+
+void resetData() {
+  //Value 1000 - 2000, 1500 is middle/neutral
+  data.Throttle = 1000;
+  data.Yaw = 1500;
+  data.Pitch = 1500;
+  data.Roll = 1500;
+  data.Aux1 = 1000;
+  data.Aux2 = 1000;
+  data.Aux3 = 1000;
+  data.Aux4 = 1000;
+  data.Aux5 = 1000;
+  data.Aux6 = 1000;
+
+  setPPMValuesFromData();
+}
+
+void setPPMValuesFromData(){
+  PPM[0] = data.Throttle;
+  PPM[1] = data.Yaw;
+  PPM[2] = data.Pitch;
+  PPM[3] = data.Roll;
+  PPM[4] = data.Aux1;
+  PPM[5] = data.Aux2;
+  PPM[6] = data.Aux3;
+  PPM[7] = data.Aux4;
+  PPM[8] = data.Aux5;
+  PPM[9] = data.Aux6;
+}
+
+void setupPPM() {
+  //digitalWrite(PPM_PIN, 0);  //set the PPM signal pin to the default state (off)
+  PORTD = PORTD & ~B00000100;
+  
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+
+  OCR1A = 100;  // compare match register (not very important, sets the timeout for the first interrupt)
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+}
+
+ISR(TIMER1_COMPA_vect) {
+  static boolean state = true;
+  TCNT1 = 0;
+#ifdef DEBUG_MODE
+    Serial.print("PPM-Throttle: ");     Serial.println(PPM[0]);
+#endif
+  if ( state ) {
+    //end pulse
+    PORTD = PORTD & ~B00000100; // turn pin 2 off. Could also use: digitalWrite(sigPin,0)
+    OCR1A = PPM_PULSE_LENGTH * CLOCK_MULTIPLIER;
+    state = false;
+  }else {
+    //start pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+
+    PORTD = PORTD | B00000100; // turn pin 2 on. Could also use: digitalWrite(sigPin,1)
+    state = true;
+
+    if (cur_chan_numb >= CHANNEL_COUNT) {
+      cur_chan_numb = 0;
+      calc_rest += PPM_PULSE_LENGTH;
+      OCR1A = (PPM_FRAME_LENGTH - calc_rest) * CLOCK_MULTIPLIER;
+      calc_rest = 0;
+    }
+    else {
+      OCR1A = (PPM[cur_chan_numb] - PPM_PULSE_LENGTH) * CLOCK_MULTIPLIER;
+      calc_rest += PPM[cur_chan_numb];
+      cur_chan_numb++;
+    }
+  }
+}
+
